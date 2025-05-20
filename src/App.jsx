0057ed57d4b21f8eaa45a30e3d869c8ec93144c1
exe1379 <exe1379@gmail.com>
2025-05-20 10:32:08 +0800

    create

---
+++ src/App.jsx
@@ -0,0 +1,87 @@
+import './App.css'
+import { useState, useEffect } from 'react';
+
+const API_BASE = 'http://localhost:8081/rest/room'
+
+function App() {
+
+  const [rooms, setRooms] = useState([]);
+  
+  
+  useEffect(() => {fetchRooms();},[])
+
+  const fetchRooms = async () => {
+    try{
+      const res = await fetch(API_BASE, {credentials: 'include'})
+      const data = await res.json();
+      console.log('data:',data);
+      setRooms(data.data);
+    }catch(err){
+      console.log('取得房間資料失敗', err.message);
+      alert('取得房間資料失敗' + err.message)
+    }
+  }
+
+  return (
+    <div style={{ padding: '20px', fontFamily: 'Arial' }}>
+      <h1>房間管理系統</h1>
+
+      <form style={{ marginBottom: '30px' }}>
+        <fieldset>
+          <legend>新增房間</legend>
+          <div>
+            <label>房號：</label>
+            <input
+              type="number"
+              name="roomId"
+              required
+            />
+          </div>
+          <div>
+            <label>名稱：</label>
+            <input
+              type="text"
+              name="roomName"
+            />
+          </div>
+          <div>
+            <label>人數：</label>
+            <input
+              type="number"
+              name="roomSize"
+              required
+            />
+          </div>
+          <button type="submit">新增房間</button>
+        </fieldset>
+      </form>
+      <table border="1" cellPadding="10">
+        <thead>
+          <tr>
+            <th>房號</th>
+            <th>名稱</th>
+            <th>人數</th>
+            <th>編輯</th>
+            <th>刪除</th>
+          </tr>
+        </thead>
+        <tbody>
+            {rooms.map((room) => (
+              <tr key='room.roomId'>
+               <td>{room.roomId}</td> 
+               <td>{room.roomName}</td> 
+               <td>{room.roomSize}</td> 
+               <td>
+                <button type='submit'>編輯</button>
+               </td>
+               <td>
+                <button type='submit'>刪除</button>
+               </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+export default App;
